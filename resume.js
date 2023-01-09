82fd54955db8bbf233004cd22131058d6cb955c2
Sivakumar0703 <121884356+Sivakumar0703@users.noreply.github.com>
2023-01-09 23:29:11 +0530

    day03_resume
    day03_resume

---
+++ resume.js
@@ -0,0 +1,45 @@
+let resume = {
+    "empty-line1" : "",
+    "Horizontal_line0" : "__________________________________________________________________________________________________________________________",
+    "Name" : "SIVAKUMAR S",
+   // "empty-line2" : "",
+    "Address" : "No 02 xyz street,xyz Nagar,Chennai 600-001",
+   // "Phone": "Mobile:9876543210                  Email:someone@gmail.com                 Linkedin: linkedin.com/in/abcd", 
+    "Phone1": "Mobile:9876543210 | Email:someone@gmail.com | Linkedin: linkedin.com/in/abcd", 
+    "Horizontal_line" : "__________________________________________________________________________________________________________________________",
+    "Career objective" : "CAREER OBJECTIVE:",
+    "Career objective1" : "I'm seeking employment with a company Where i am fully use my skills for the success of the organization.To enhance my professional skills in stable workplace.",
+    "Horizontal_line1" : "__________________________________________________________________________________________________________________________",
+    "edu" : "EDUCATIONAL QUALIFICATION:",
+    "Education"      : "XYZ ENGINEERING COLLEGE  , Mechanical Engineering with 68% , 2018.",
+    "Education_hsc"  : "XYZ Matric Hr Sec School , HSC with 76%                    , 2014.",
+    "Education_sslc" : "XYZ Matric Hr Sec School , SSLC with 76%                   , 2012.",
+    "Horizontal_line2" : "__________________________________________________________________________________________________________________________",
+    "Project"      : "PROJECT:",
+    "project1"          : "1.Analysis of soot blower and water lancer in boiler system at NLC.",
+    "project2"          : "2. Emergency braking system by auto measuring distance.",
+    "Horizontal_line3" : "__________________________________________________________________________________________________________________________",
+    "INTEREST"         : "INTEREST:",
+    "interest1"              : "1.THERMAL",
+    "interest2"              : "2.Automobile",
+    "Horizontal_line4" : "__________________________________________________________________________________________________________________________",
+    "Skills acquired"   : "TECHNICAL SKILLS:",
+    "skill1"            : "1.Auto-Cad",
+    "skill2"            : "2.Catia",
+    "skill3"            : "3.Unigraphics",
+    "Horizontal_line5" : "__________________________________________________________________________________________________________________________",
+    "strength"           : "STRENGTH:",
+    "strength1"           : "Self-motivated",
+    "strength2"           : "Flexible",
+    "Horizontal_line6" : "__________________________________________________________________________________________________________________________",
+    "Place" : "Place :",
+    "Date"  : "Date  :                                                                                          Signature"
+
+
+}
+
+for(var i in resume){
+    console.log(resume[i]);
+}
+
+   
